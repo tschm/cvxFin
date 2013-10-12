49403d9acde30451d5e317a3daaefeb662db7cc9
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2013-10-12 16:40:26 +0200

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='cvxFin',
+    version='1.0',
+    packages=['util'],
+    url='https://github.com/tschm/cvxFin',
+    license='',
+    author='Thomas Schmelzer',
+    author_email='thomas.schmelzer@gmail.com',
+    description=''
+)
