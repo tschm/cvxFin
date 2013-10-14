dfcf875da3460db54ec046d0054656940a5f6d08
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2013-10-14 16:09:30 +0200

    makedoc sphinx

--- makedoc.py
+++ makedoc.py
@@ -1,6 +1,6 @@
 import os
 if __name__ == '__main__':
-    os.system("sphinx-apidoc -F -H cvxFin -A \"Thomas Schmelzer\" -V 1.0 -R 1.0.0 -o doc cvxFin")
+    #os.system("sphinx-apidoc -F -H cvxFin -A \"Thomas Schmelzer\" -V 1.0 -R 1.0.0 -o doc cvxFin")
     os.chdir("doc")
     os.system("make clean")
     os.system("make html")
