ded9d93875d27a0746c9b5bb0a2896c09df615bc
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2013-10-14 14:25:06 +0200

    packages

---
+++ cvxFin/cvx/tests/testCvxFinance.py
@@ -0,0 +1,139 @@
+from nose.tools import with_setup
+from nose.tools import assert_raises
+
+from cvxopt import solvers
+
+import numpy as np
+import numpy.testing as npTest
+
+import cvxFin.cvx.cvxFinance as Fin
+
+
+def setup_func():
+    solvers.options['show_progress'] = False
+
+
+def teardown_func():
+    pass
+
+
+def __get_c():
+    return np.array([3.0, 1.0, 5.0, 1.0])
+
+
+def __get_A():
+    return np.array([[3.0, 1.0, 2.0, 0.0],
+                     [2.0, 1.0, 3.0, 1.0],
+                     [0.0, 2.0, 0.0, 3.0]])
+
+
+def __get_Q():
+    return np.array([[1.0, 0.3, 0.2, 0.2],
+                     [0.3, 1.0, 0.25, 0.5],
+                     [0.2, 0.25, 1.0, 0.4],
+                     [0.2, 0.5, 0.4, 1.0]])
+
+
+def __get_bxl():
+    return np.zeros(4)
+
+
+def __get_bxu():
+    return np.array([np.inf, 10.0, np.inf, np.inf])
+
+
+def __get_bcl():
+    return np.array([30.0, 15.0, -np.inf])
+
+
+def __get_bcu():
+    return np.array([30.0, np.inf, 25.0])
+
+
+@with_setup(setup_func, teardown_func)
+def testLinear():
+    #see the example at
+    #http://docs.mosek.com/6.0/capi/node007.html#250240248
+    y = Fin.solveLP(c=__get_c(), A=__get_A(), bxl=__get_bxl(), bxu=__get_bxu(), bcl=__get_bcl(), bcu=__get_bcu())
+    npTest.assert_array_almost_equal(y, np.array([0, 0, 15.0, 8.333333]))
+
+
+@with_setup(setup_func, teardown_func)
+def testLinearFalseBounds():
+    assert_raises(AssertionError, Fin.solveLP, c=np.array([1.0]), A=[], bxl=np.array([2.0]), bxu=np.array([0.0]), bcl=[], bcu=[])
+
+
+@with_setup(setup_func, teardown_func)
+def testLinearBoundsTooShort():
+    assert_raises(AssertionError, Fin.solveLP,
+                  c=np.array([1.0, 2.0]),
+                  A=[],
+                  bxl=np.array([2.0]),
+                  bxu=np.array([0.0]),
+                  bcl=[],
+                  bcu=[])
+
+
+@with_setup(setup_func, teardown_func)
+def testLinearNoConstraints():
+    assert_raises(ArithmeticError, Fin.solveLP,
+                  c=np.array([1.0]),
+                  A=np.array([1.0]),
+                  bxl=np.array([-np.inf]),
+                  bxu=np.array([np.inf]),
+                  bcl=np.array([-np.inf]),
+                  bcu=np.array([np.inf]))
+
+
+@with_setup(setup_func, teardown_func)
+def testLinearInfeasible():
+    assert_raises(ArithmeticError, Fin.solveLP,
+                  c=np.array([1.0, 1.0]),
+                  A=np.array([[1.0, 0.0], [1.0, 1.0]]),
+                  bxl=np.array([0.0, 0.0]),
+                  bxu=np.array([1.0, 1.0]),
+                  bcl=np.array([3.0, 3.0]),
+                  bcu=np.array([5.0, 5.0]))
+
+
+@with_setup(setup_func, teardown_func)
+def testQuadratic():
+    x = Fin.solveQPcon(c=__get_c(), A=__get_A(), Q=__get_Q(), qc=17, bxl=__get_bxl(), bxu=__get_bxu(), bcl=__get_bcl(), bcu=__get_bcu())
+    npTest.assert_array_almost_equal(x, np.array([0, 0, 15.0, 4.00]))
+
+
+@with_setup(setup_func, teardown_func)
+def testQuadNegative():
+    assert_raises(AssertionError, Fin.solveQPcon, c=__get_c(), A=__get_A(), Q=__get_Q(), qc=-17,
+                  bxl=__get_bxl(), bxu=__get_bxu(), bcl=__get_bcl(), bcu=__get_bcu())
+
+
+@with_setup(setup_func, teardown_func)
+def testQuadInfeasible():
+    assert_raises(ArithmeticError, Fin.solveQPcon, c=__get_c(), A=__get_A(), Q=__get_Q(), qc=1,
+                  bxl=np.array([2.0, 2.0, 2.0, 2.0]), bxu=np.array([5.0, 5.0, 5.0, 5.0]), bcl=__get_bcl(), bcu=__get_bcu())
+
+
+@with_setup(setup_func, teardown_func)
+def testMarkowitzConstraint():
+    v1 = 2.0*np.ones(4)
+    x0 = np.array([5.0, 1.0, 10.0, 2.0])
+
+    x = Fin.solveMarkowitzConstraint(c=__get_c(), A=__get_A(), Q=__get_Q(), qc=17, v=v1, x0=x0, bxl=__get_bxl(), bxu=__get_bxu(), bcl=__get_bcl(), bcu=__get_bcu())
+    npTest.assert_array_almost_equal(x, np.array([3.0, 1.0, 10.0, 2.0]))
+
+
+@with_setup(setup_func, teardown_func)
+def testMarkowitzObjective():
+    v1 = 0.5*np.ones(4)
+    x0 = np.array([5.0, 1.0, 10.0, 2.0])
+
+    x = Fin.solveMarkowitzObjective(c=__get_c(), A=__get_A(), Q=__get_Q(), v=v1, x0=x0, bxl=__get_bxl(), bxu=__get_bxu(), bcl=__get_bcl(), bcu=__get_bcu())
+    npTest.assert_array_almost_equal(x, np.array([5.18867943e+00, 1.11254698e-06, 7.21698030e+00, 2.46512400e-08]))
+
+
+@with_setup(setup_func, teardown_func)
+def testQuadraticObjective():
+    x = Fin.solveQPobj(c=__get_c(), A=__get_A(), Q=__get_Q(), bxl=__get_bxl(), bxu=__get_bxu(), bcl=__get_bcl(), bcu=__get_bcu())
+    npTest.assert_array_almost_equal(x, np.array([5.66037740e+00, 7.27653920e-08, 6.50943386e+00, 2.54269440e-07]))
+
---
+++ cvxFin/util/markowitzProgram.py
@@ -0,0 +1,67 @@
+import numpy
+from cvxFin.util.cone import conelp, coneqp
+
+from cvxFin.util.quadraticProgram import QuadraticConstraintProgram, QuadraticObjectiveProgram
+
+
+class AbsPenalty:
+    def __init__(self, n):
+        self.v = numpy.zeros(n)
+        self.x0 = numpy.zeros(n)
+
+    def getCVX(self):
+        h = numpy.concatenate([self.x0, -self.x0])
+        E = numpy.eye(len(self.x0), len(self.x0))
+        G = numpy.vstack([numpy.hstack([E, -E]), numpy.hstack([-E, -E])])
+        return {'mat': G, 'vec': h}
+
+    def eval(self, x):
+        return numpy.sum(self.v * numpy.abs(x - self.x0))
+
+
+class MarkowitzObjectiveProgram:
+    def __init__(self, Q, c, A):
+        self.qp = QuadraticObjectiveProgram(Q=Q, p=c, A=A)
+        self.penalty = AbsPenalty(len(c))
+
+    def solve(self):
+        c = numpy.concatenate([self.qp.lp.c, self.penalty.v])
+        Q = numpy.zeros((len(c), len(c)))
+        Q[0:len(self.qp.lp.c), 0:len(self.qp.lp.c)] = self.qp.Q
+
+        X = coneqp(P=Q, q=c)
+
+        bx = self.qp.lp.bx.getCVX()
+        bc = self.qp.lp.bc.getCVX()
+        p = self.penalty.getCVX()
+
+        X.aux.pushLinConstraint(numpy.hstack([bx['mat'], numpy.zeros(bx['mat'].shape)]), bx['vec'])
+        X.aux.pushLinConstraint(numpy.hstack([bc['mat'], numpy.zeros(bc['mat'].shape)]), bc['vec'])
+        X.aux.pushLinConstraint(p['mat'], p['vec'])
+
+        x = X.solve()
+        return x[0][0:len(self.qp.lp.c)], x[1]
+
+
+class MarkowitzConstraintProgram:
+    def __init__(self, c, A, Q):
+        self.qp = QuadraticConstraintProgram(c, A, Q)
+        self.penalty = AbsPenalty(len(c))
+
+    def solve(self):
+        c = numpy.concatenate([self.qp.lp.c, self.penalty.v])
+
+        X = conelp(c)
+
+        bx = self.qp.lp.bx.getCVX()
+        bc = self.qp.lp.bc.getCVX()
+        qc = self.qp.qc.getCVX()
+        p = self.penalty.getCVX()
+
+        X.aux.pushLinConstraint(numpy.hstack([bx['mat'], numpy.zeros(bx['mat'].shape)]), bx['vec'])
+        X.aux.pushLinConstraint(numpy.hstack([bc['mat'], numpy.zeros(bc['mat'].shape)]), bc['vec'])
+        X.aux.pushLinConstraint(p['mat'], p['vec'])
+        X.aux.pushQuadConstraint(numpy.hstack([qc['mat'], numpy.zeros(qc['mat'].shape)]), qc['vec'])
+
+        x = X.solve()
+        return x[0][0:len(self.qp.lp.c)], x[1]
