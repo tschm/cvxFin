e29894908c81568e2000e3ccc4c7c39deb99499e
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-11-07 15:14:51 +0100

    dockerizing cvx

--- cvxFin/cvx/cvxRegression.py
+++ cvxFin/cvx/cvxRegression.py
@@ -4,23 +4,7 @@ import cvxFin.cvx.util as ccu
 import cvxpy as cvx
 
 
-def solveLSQ(b, A, B, bxl, bxu, bcl, bcu):
-    """
-    Solves a constrained linear least squares problem
-
-    minimize   2-norm (A*x-b)
-    subject to  bcl <= B*x <= bcu
-                bxl <=  x  <= bxu
-    """
-    (n, m) = A.shape
-    x = cvx.Variable(m)
-    constraints = [B*x <= bcu, bcl <= B*x, bxl <= x, x <= bxu]
-    objective = cvx.norm(A*x-b,2)
-    ccu.minimize(objective=objective, constraints=constraints)
-    return ccu.cvx2np(x)
-
-
-def solveLASSO(b, A, B, bxl, bxu, bcl, bcu, tau):
+def solveLASSO(b, A, B, bxl, bxu, bcl, bcu, tau=0):
     """
     Solves a constrained linear least squares problem
 
